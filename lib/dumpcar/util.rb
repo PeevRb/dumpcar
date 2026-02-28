fba10c1d46cb57dd87cd718e67ee6bbb1cbc353e
Eric Schultz <eric@wwahammy.com>
2026-02-28 20:42:45 +0000

    Move to backported broadcast_logger

--- lib/dumpcar/util.rb
+++ lib/dumpcar/util.rb
@@ -3,7 +3,17 @@ module Dumpcar::Util
     (Rails.version < "6.1") ? ActiveRecord::Base.connection_config : ActiveRecord::Base.connection_db_config.configuration_hash
   end
 
+  def self.logger_class
+    if Rails.version < "7.1"
+      # We only load this if we needed it
+      require "dumpcar/util/broadcast_logger"
+      Dumpcar::Util::BroadcastLogger
+    else
+      ActiveSupport::BroadcastLogger
+    end
+  end
+
   def self.logger
-    Rails.logger
+    logger_class.new(Rails.logger, Logger.new($stdout))
   end
 end
