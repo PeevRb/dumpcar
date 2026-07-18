f65f530fa69da11c37665d8cd500b312c9fb8e25
Eric Schultz <eric@wwahammy.com>
2026-07-18 01:53:59 +0000

    Correct the ordering

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@
 
 require "bundler"
 
+Bundler.require :default, :development
+
 require "simplecov"
 
 if ENV["CI"]
@@ -11,8 +13,6 @@ if ENV["CI"]
 end
 
 SimpleCov.start
-
-Bundler.require :default, :development
 
 Combustion.initialize! :active_record
 
